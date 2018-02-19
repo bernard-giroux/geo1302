86358a99e52d4c9debd588f5467d3cb680a3fd2e
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2018-02-19 10:27:57 -0500

    now compatible with python 3.6

--- geo1302/utils.py
+++ geo1302/utils.py
@@ -5,6 +5,7 @@ Created on Sun Jan 15 19:54:19 2017
 @author: giroux
 """
 
+import sys
 import inspect,dis
 
 def nargout():
@@ -14,14 +15,18 @@ def nargout():
     taken from
     http://stackoverflow.com/questions/16488872/python-check-for-the-number-of-output-arguments-a-function-is-called-with
     """
+    if sys.version_info[1] > 5:
+        off = 1
+    else:
+        off = 0
     f = inspect.currentframe()
     f = f.f_back.f_back
     c = f.f_code
     i = f.f_lasti
     bytecode = c.co_code
-    instruction = bytecode[i+3]
+    instruction = bytecode[i+3-off]
     if instruction == dis.opmap['UNPACK_SEQUENCE']:
-        howmany = bytecode[i+4]
+        howmany = bytecode[i+4-off]
         return howmany
     elif instruction == dis.opmap['POP_TOP']:
         return 0

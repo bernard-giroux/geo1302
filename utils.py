96614213b8c278e5ae031b95c9cbbe44700d8835
Bernard Giroux <bernard.giroux@ete.inrs.ca>
2020-02-17 10:35:48 -0500

    fct utils ajoutee

---
+++ utils.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 15 19:54:19 2017
+
+@author: giroux
+"""
+
+import sys
+import inspect,dis
+
+import numpy as np
+from numpy.linalg import svd
+
+def nargout():
+    """
+    Return how many values the caller is expecting
+
+    taken from
+    http://stackoverflow.com/questions/16488872/python-check-for-the-number-of-output-arguments-a-function-is-called-with
+    """
+    if sys.version_info[1] > 5:
+        off = 1
+    else:
+        off = 0
+    f = inspect.currentframe()
+    f = f.f_back.f_back
+    c = f.f_code
+    i = f.f_lasti
+    bytecode = c.co_code
+    instruction = bytecode[i+3-off]
+    if instruction == dis.opmap['UNPACK_SEQUENCE']:
+        howmany = bytecode[i+4-off]
+        return howmany
+    elif instruction == dis.opmap['POP_TOP']:
+        return 0
+    return 1
+
+
+def rank(A, atol=1e-13, rtol=0):
+    """Estimate the rank (i.e. the dimension of the nullspace) of a matrix.
+
+    The algorithm used by this function is based on the singular value
+    decomposition of `A`.
+
+    Parameters
+    ----------
+    A : ndarray
+        A should be at most 2-D.  A 1-D array with length n will be treated
+        as a 2-D with shape (1, n)
+    atol : float
+        The absolute tolerance for a zero singular value.  Singular values
+        smaller than `atol` are considered to be zero.
+    rtol : float
+        The relative tolerance.  Singular values less than rtol*smax are
+        considered to be zero, where smax is the largest singular value.
+
+    If both `atol` and `rtol` are positive, the combined tolerance is the
+    maximum of the two; that is::
+        tol = max(atol, rtol * smax)
+    Singular values smaller than `tol` are considered to be zero.
+
+    Return value
+    ------------
+    r : int
+        The estimated rank of the matrix.
+
+    See also
+    --------
+    numpy.linalg.matrix_rank
+        matrix_rank is basically the same as this function, but it does not
+        provide the option of the absolute tolerance.
+
+    From: http://scipy-cookbook.readthedocs.io/items/RankNullspace.html
+    """
+
+    A = np.atleast_2d(A)
+    s = svd(A, compute_uv=False)
+    tol = max(atol, rtol * s[0])
+    rank = int((s >= tol).sum())
+    return rank
+
+
+def nullspace(A, atol=1e-13, rtol=0):
+    """Compute an approximate basis for the nullspace of A.
+
+    The algorithm used by this function is based on the singular value
+    decomposition of `A`.
+
+    Parameters
+    ----------
+    A : ndarray
+        A should be at most 2-D.  A 1-D array with length k will be treated
+        as a 2-D with shape (1, k)
+    atol : float
+        The absolute tolerance for a zero singular value.  Singular values
+        smaller than `atol` are considered to be zero.
+    rtol : float
+        The relative tolerance.  Singular values less than rtol*smax are
+        considered to be zero, where smax is the largest singular value.
+
+    If both `atol` and `rtol` are positive, the combined tolerance is the
+    maximum of the two; that is::
+        tol = max(atol, rtol * smax)
+    Singular values smaller than `tol` are considered to be zero.
+
+    Return value
+    ------------
+    ns : ndarray
+        If `A` is an array with shape (m, k), then `ns` will be an array
+        with shape (k, n), where n is the estimated dimension of the
+        nullspace of `A`.  The columns of `ns` are a basis for the
+        nullspace; each element in numpy.dot(A, ns) will be approximately
+        zero.
+
+    From: http://scipy-cookbook.readthedocs.io/items/RankNullspace.html
+    """
+
+    A = np.atleast_2d(A)
+    u, s, vh = svd(A)
+    tol = max(atol, rtol * s[0])
+    nnz = (s >= tol).sum()
+    ns = vh[nnz:].conj().T
+    return ns
+
+
+if __name__ == '__main__':
+    
+    def dummy():
+        nout = nargout()
+        print(nout)
+        return tuple([x for x in range(nout)])
+    
+    toto = dummy()
+    tata, titi, tutu = dummy()
+    tete, toto = dummy()
+    
